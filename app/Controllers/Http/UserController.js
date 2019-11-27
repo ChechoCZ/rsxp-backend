63045edb70b56e2f05708be3e96dd1b0be60e566
Lucas Alves <lucas.alves@udf.org.br>
2019-11-27 12:29:39 -0300

    ajustes rota de usuario

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,41 +1,43 @@
-'use strict'
+"use strict";
 
-const User = use('App/Models/User')
-const Course = use('App/Models/Course')
+const User = use("App/Models/User");
+const Course = use("App/Models/Course");
 
 class UserController {
-  
-  async index ({ request, response, view }) {
-    const user = await User.all()
+  async index({ request, response, view }) {
+    const user = await User.all();
 
-    return user
+    return user;
   }
 
-  async store ({ request, response }) {
-    const user = await User.create(request.body)
-    const courses = await Course.all()
+  async store({ request, response }) {
+    const user = await User.create(request.body);
 
-    await user.courses().attach(courses.toJSON().map(course => course.id))
+    const courses = await Course.all();
 
-    const user_courses = await user.courses().fetch()
+    await user.courses().attach(courses.toJSON().map(course => course.id));
 
-    return user_courses
+    await user.courses().fetch();
+
+    return user;
   }
 
-  async show ({ params, request, response, view }) {
-    const user = await User.query().where('id', '=', request.body.id).with('courses').fetch()
+  async show({ params, request, response, view }) {
+    const user = await User.query()
+      .where("id", "=", request.body.id)
+      .with("courses")
+      .fetch();
 
-    return user
+    return user;
   }
 
-  async update ({ params, request, response }) {
-  }
+  async update({ params, request, response }) {}
 
-  async destroy ({ params, request, response }) {
-    const user = await User.find(params.id)
+  async destroy({ params, request, response }) {
+    const user = await User.find(params.id);
 
-    user.delete()
+    user.delete();
   }
 }
 
-module.exports = UserController
+module.exports = UserController;
